9eb4c450f5ef56ae25285f36c53685ddb9dcca9a
madhu7894 <madhu@netprise.co.in>
2020-09-18 17:49:58 +0530

    Updates

---
+++ precache-manifest.d84cb7ab33413635d9324692132975d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2614f205ab11e8e257a009779644f89",
+    "url": "/Assignment1/index.html"
+  },
+  {
+    "revision": "7348320779f91ba41bc8",
+    "url": "/Assignment1/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d77decfc53b4f1b2c132",
+    "url": "/Assignment1/static/js/2.8a02b675.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Assignment1/static/js/2.8a02b675.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7348320779f91ba41bc8",
+    "url": "/Assignment1/static/js/main.cbd66cc2.chunk.js"
+  },
+  {
+    "revision": "dbe297d388bb69a7955e",
+    "url": "/Assignment1/static/js/runtime-main.48b10ae6.js"
+  }
+]);
